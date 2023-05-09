2e678b80dc78224207f296c875f8007770521ee0
Vitalii Smyrnov <113116677+Vetal-Smyrnov@users.noreply.github.com>
2023-05-09 19:35:07 +0300

    push

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  width: 280px;
+  border: 5px solid blue;
+  border-radius: 5%;
+  background-color: yellow;
+`;
